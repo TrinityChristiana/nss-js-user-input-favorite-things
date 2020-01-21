e181a50af00e70db60121a932ba1f43b540a71c2
Trinity Terry <trinitycterry@gmail.com>
2020-01-20 20:45:38 -0600

    added validation

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
-# Name of Repo
-
+# nss-js-user-input-favorite-things
 Date Created: 01/00/20 | Date Modified: 01/00/20
 
 This assignment is practice for Book 0: Chapter 0 of NSS's course work. Link to the assignment: 
 
 Preview Assignment: 
 ***
-# nss-js-user-input-favorite-things
+
+Practice: These are a few of my favorite things
+1. Build two fieldsets and input fields that will allow you to enter the name of a favorite thing of yours, and a location where you might purchase it (Target, Amazon, Best Buy, etc...)
+
+2. Add a button beneath the fieldsets labeled "Save to Wishlist".
+
+3. When the button is clicked, display the data in the following format in the DOM.
+
+```I can purchase {thing} at {location}
+```
---
+++ script.js
@@ -0,0 +1,46 @@
+let getInputValue = () => {
+    let thing = document.querySelector("#thing").value;
+    let place = document.querySelector("#place").value;
+    return [thing, place];
+};
+
+let writeText = (textArray) => `I can purchase ${textArray[0]} at ${textArray[1]}`;
+
+let renderText = (text) => {
+    let listContainer = document.querySelector(".list-container");
+    let item = document.createElement("div");
+    item.textContent = text;
+    listContainer.appendChild(item);
+}
+
+let clearTextBox = () => {
+    // let textInput = document.getElementByName("favoriteForm");
+    document.forms.favoriteForm.reset();
+}
+
+let wishButton = document.querySelector("#saveItem");
+
+wishButton.addEventListener("click", (e) => {
+
+    let thing = document.getElementById("thing");
+    let place = document.getElementById("place");
+
+    if (!(/\S/.test(thing.value)) || !(/\S/.test(place.value))) {
+        // clearTextBox();
+        alert("Please enter in all information")
+        e.preventDefault();
+    } else if ((thing.value !== "") || (place.value !== "")) {
+        let textArray = getInputValue();
+        let domText = writeText(textArray);
+        renderText(domText);
+        clearTextBox();
+        thing.focus();
+        e.preventDefault();
+        
+    }
+
+    
+});
+
+
+
